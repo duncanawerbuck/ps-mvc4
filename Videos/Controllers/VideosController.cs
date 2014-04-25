258212ff6025885dff9d2b92157614d1bab5a5ae
Duncan Awerbuck <duncan.awerbuck@aon.ie>
2014-04-25 15:09:21 +0100

    Added VS MVC4 Internet App

---
+++ Videos/Controllers/VideosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Videos.Controllers
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } 
+    }
+
+    public class VideosController : ApiController
+    {
+        // GET api/video
+        public IEnumerable<string> GetAllVideos()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/video/5
+        public string Get(int id)
+        {
+            return "value " + id;
+        }
+
+        // POST api/video
+        public Video Post(Video video)
+        {
+            return video;
+        }
+
+        // PUT api/video/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/video/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Videos/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Videos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aon")]
+[assembly: AssemblyProduct("Videos")]
+[assembly: AssemblyCopyright("Copyright © Aon 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bcca9b8a-a8e5-4ec9-b0af-8fe4f318ca42")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
